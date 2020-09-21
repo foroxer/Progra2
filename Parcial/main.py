72ff9ce70e56632f57afc0dde7675bb940a28082
foroxer <juancho_93_22@hotmail.com>
2020-09-21 20:06:43 -0300

    final

--- Parcial/main.py
+++ Parcial/main.py
@@ -1,6 +1,7 @@
 class Personaje():
 
-    def __init__(self, vida=100, posicion=0, velocidad=0):
+    def __init__(self,nombre:str ,  vida=100, posicion=0, velocidad=0):
+        self.nombre = nombre
         self.vida = vida
         self.posicion = posicion
         self.velocidad = velocidad
@@ -8,67 +9,81 @@ class Personaje():
     def recibir_ataque(self, cantidad):
         if (cantidad < self.vida):
             self.vida -= cantidad
-            print('recibio un ataque, su vida quedo en ' + str(self.vida))
+            print(self.nombre + ' recibio un ataque, su vida quedo en ' + str(self.vida))
         else:
             self.vida = 0
-            print('el personaje se quedo sin vida')
+            print('el personaje ' + self.nombre + ' se quedo sin vida')
 
-    def mover(self, destino, velocidad):
-        if (velocidad <= 0): return
-        print('moviendose desde ' + str(self.posicion) + ' hasta ' + str(destino) + ' con una velocidad de ' + str(velocidad))
+    def mover(self, destino):
+        if (self.velocidad <= 0):
+            return
+        print(self.nombre +' esta moviendose desde ' + str(self.posicion) + ' hasta ' +
+              str(destino) + ' con una velocidad de ' + str(self.velocidad))
         # implementar-----------------------------------------------------------------
-        loops = int(abs((self.posicion-destino))/velocidad)
+        loops = int(abs((self.posicion-destino))/self.velocidad)
         if (self.posicion > destino):
             for _ in range(loops):
                 print(self.posicion)
-                self.posicion -= velocidad
+                self.posicion -= self.velocidad
             print(self.posicion)
             if (self.posicion != destino):
                 self.posicion = destino
                 print(self.posicion)
+            print(self.nombre + ' llego a destino')
 
         elif(self.posicion < destino):
             for _ in range(loops):
                 print(self.posicion)
-                self.posicion += velocidad
+                self.posicion += self.velocidad
             print(self.posicion)
             if (self.posicion != destino):
                 self.posicion = destino
                 print(self.posicion)
-            
+            print(self.nombre + ' llego a destino')
+
         else:
-            print('ud ya se encuentra en esa posicion')
-    
+            print(self.nombre + ' ya se encuentra en esa posicion')
+
+
 class Soldado(Personaje):
-    
-    def __init__(self,vida = 100, posicion = 0, velocidad=0, ataque=0):
-        super().__init__(vida, posicion ,velocidad)
+
+    def __init__(self,nombre:str, vida=100, posicion=0, velocidad=4, ataque=10):
+        super().__init__(nombre,vida, posicion, velocidad)
         self.ataque = ataque
-    
+
     def atacar(self, personaje: Personaje):
-        print('Atacando')
+        print(self.nombre + 'esta atacando a :' +personaje.nombre)
         personaje.recibir_ataque(self.ataque)
 
 
 class Campesino(Personaje):
-    def __init__(self,vida = 100, posicion = 0, velocidad=0, cosecha =0):
-        super().__init__(vida, posicion ,velocidad)
-        self.cosecha  = cosecha 
-    
+    def __init__(self,nombre:str, vida=100, posicion=0, velocidad=2, cosecha=0):
+        super().__init__(nombre,vida, posicion, velocidad)
+        self.cosecha = cosecha
+
     def cosechar(self):
-        print('La cantidad cosechada fue de '+ self.cosecha)
+        print('La cantidad cosechada por ' + self.nombre + ' fue de ' + str(self.cosecha))
         return self.cosecha
 
 
+print('---------------------------------------------------------')
+print('El campesino puede moverse, cosechar y recibir un ataque')
+c = Campesino('Campesino')
+c.cosecha = 20
+c.cosechar()
+c.mover(9)
+c.recibir_ataque(10)
 
 
-s = Soldado()
-c = Campesino()
-s.mover(-9,1)
-s.mover(10,2)
-s.ataque = 70
+print('---------------------------------------------------------')
+print('El soldado puede moverse, atacar y recibir un ataque')
+s1 = Soldado('Soldado 1')
+s2 = Soldado('Soldado 2')
 
-s.atacar(c)
-s.ataque = 40
-s.atacar(c)
+s1.mover(-9)
+s1.mover(10)
 
+s1.atacar(c)
+s1.atacar(s2)
+s1.recibir_ataque(110)
+print('---------------------------------------------------------')
