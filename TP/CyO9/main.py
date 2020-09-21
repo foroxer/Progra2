c452c0a97dd3565c53715929b3eaae41b15a8e17
foroxer <juancho_93_22@hotmail.com>
2020-09-21 00:17:00 -0300

    ej9

--- TP/CyO9/main.py
+++ TP/CyO9/main.py
@@ -1,1 +1,18 @@
 #Realice un ejemplo en código donde se utilicen los métodos especiales init, new, del, str y cmp.
+#el metodo cmp no existe en python3
+class A(object):
+    var = 123
+
+    def __new__(cls):
+        print("new")
+        return super(A, cls).__new__(cls)
+
+    def __init__(self):
+        print("init")
+
+    def __del__(self):
+        print("del")
+
+a = A()
+print("string e la variable numerica : " + str(a.var))
+del a
